81cac230fe4a3be0522f08bed147e2766c221ae6
Paris <117606280+prs2772@users.noreply.github.com>
2026-03-06 23:21:06 -0600

    AVF-002 Add environments for APIs requests

---
+++ astra-ventura-f-main/src/environments/apis.environment.ts
@@ -0,0 +1,6 @@
+export const apis = {
+    production: false,
+    authApiUrl: 'https://astra-ventura-auth-api.blackpond-dc411ebf.eastus2.azurecontainerapps.io',
+    notebookApiUrl: 'https://autofleet-ezdrgae0gch8egbn.canadacentral-01.azurewebsites.net',
+    vehiculosApiUrl: 'https://astra-ventura-vehiculos-api.blackpond-dc411ebf.eastus2.azurecontainerapps.io',
+};
